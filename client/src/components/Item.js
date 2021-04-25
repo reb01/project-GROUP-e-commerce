8a22a809729a5fd02470be5ad8b7dfc0358a3c13
reb01 <67109174+reb01@users.noreply.github.com>
2021-04-24 22:39:41 -0400

    Update Item.js

--- client/src/components/Item.js
+++ client/src/components/Item.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
 import Spinner from "./Tools/Spinner";
 import ErrorPage from "./ErrorPage";
 import IndividualItem from "./IndividualItem";
@@ -8,47 +7,42 @@ import { useParams } from "react-router-dom";
 import { COLORS } from "../constants";
 import styled from "styled-components";
 
-const Item = () => {
-  const { currentStore } = useSelector((state) => state.store);
+const Item = () => {  
   const [itemData, setItemData] = useState([]);
   const [company, setCompany] = useState([]);
   const [status, setStatus] = useState("idle");
   let { id } = useParams();
-  console.log(currentStore);
-  useEffect(() => {
-    setStatus("loading");
-    if (id) {
-      fetch(`/item/${id}`)
-        .then((response) => response.json())
-        .then((response) => {
-          setItemData(response.data);
-          setStatus("idle");
-        })
-        .catch(() => {
-          setStatus("error");
-        });
-    }
-  }, []);
 
   useEffect(() => {
     setStatus("loading");
     if (id) {
-      fetch(`/company/${id}`)
-        .then((response) => response.json())
-        .then((response) => {
-          setCompany(response.data);
+      Promise.all([
+        fetch(`/item/${id}`).then(response => response.json()),
+        fetch(`/company/${id}`).then(response => response.json())    
+      ]).then(([itemsData, compagnyData]) => { 
+        if (itemsData.status === 200 && compagnyData.status === 200){      
+          setItemData(itemsData.data);
+          setCompany(compagnyData.data);
           setStatus("idle");
-        })
-        .catch(() => {
-          setStatus("error");
-        });
+        }
+        else
+          setStatus("error");         
+      }).catch(() => {
+        setStatus("error");
+      });
     }
-  }, []);
-  console.log(itemData);
-  return (
+  }, [id]);
+  
+  
+  if (status=== "error"){
+    return (
+      <ErrorPage />
+    );
+  };
+
+  return ( 
     <Wrapper>
-      {status === "loading" && <Spinner />}
-      {status === "error" && <ErrorPage />}
+      {status === "loading" && <Spinner />}      
       {status === "idle" && (
         <ItemsWrapper>
           <IndividualItem
@@ -65,7 +59,7 @@ const Item = () => {
           />
         </ItemsWrapper>
       )}
-    </Wrapper>
+    </Wrapper>   
   );
 };
 
@@ -76,12 +70,10 @@ const Wrapper = styled.div`
   color: ${COLORS.primary};
   min-height: 700px;
   background-color: #0d3e5d;
-  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100' height='199' viewBox='0 0 100 199'%3E%3Cg fill='%238cb7d2' fill-opacity='0.4'%3E%3Cpath d='M0 199V0h1v1.99L100 199h-1.12L1 4.22V199H0zM100 2h-.12l-1-2H100v2z'%3E%3C/path%3E%3C/g%3E%3C/svg%3E");
   @media (max-width: 768px) and (max-height: 1018px) {
-  min-height:1000px;
-  height:100vh;
+    min-height: 1000px;
+    height: 100vh;
   }
-
 `;
 const ItemsWrapper = styled.div`
   @media (max-width: 650px) and (max-height: 850px) {
