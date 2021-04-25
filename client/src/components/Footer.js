c616ff5c9ef06486c6a2d36a5ed986fe8c62bbd2
reb01 <67109174+reb01@users.noreply.github.com>
2021-04-24 22:31:39 -0400

    Update Footer.js

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 import { COLORS } from "../constants";
 import styled from "styled-components";
 
-const Footer = () => {
+const Footer = () =>  {
   return (
     <>
       <Wrapper>
