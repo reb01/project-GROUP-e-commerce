85f4f1a93c9fb6838ecc55f8728ccaf7f3f256f9
reb01 <67109174+reb01@users.noreply.github.com>
2021-04-25 20:32:50 -0400

    Update item-reducer.js

--- client/src/reducers/item-reducer.js
+++ client/src/reducers/item-reducer.js
@@ -1,7 +1,7 @@
 const initialState = {};
 
 export default function itemReducer(state = initialState, action) {
-  console.log(Object.values(state));
+ 
   switch (action.type) {
     case "ADD_ITEM": {
       return {
@@ -32,11 +32,15 @@ export default function itemReducer(state = initialState, action) {
         },
       };
     }
+    case "CLEAR_CART": {
+      return state = initialState;
+    }
+
     default:
       return state;
   }
 }
 
-export const getStoreItemArray = (state) => { 
+export const getStoreItemArray = (state) => {
   return Object.values(state.item);
 };
