74e9fdeafe6a64c2a0ee28b85992b6e2c8cff2ad
reb01 <67109174+reb01@users.noreply.github.com>
2021-04-25 21:06:02 -0400

    Update handlers.js

--- server/handlers.js
+++ server/handlers.js
@@ -4,7 +4,8 @@ const purchases = require("./data/purchases");
 const { v4: uuidv4 } = require("uuid");
 const fs = require("fs");
 const { sortAndFilter } = require("./helpers");
-// console.log(items);
+
+
 
 const getSingleItem = (req, res) => {
   const id = req.params.id;
@@ -12,10 +13,10 @@ const getSingleItem = (req, res) => {
   const oneItem = items.find((item) => item._id == id);
 
   if (oneItem) {
-    res.status(200).json({ data: oneItem });
+    res.status(200).json({ status: 200, message: "success", data: oneItem });
   } else {
-    res.status(400).json({
-      status: 400,
+    res.status(404).json({
+      status: 404,
       message: "That item does not exist in the database",
     });
   }
@@ -24,65 +25,81 @@ const getSingleItem = (req, res) => {
 const getCompanyById = (req, res) => {
   const id = req.params.id;
   const oneItem = items.find((item) => item._id == id);
+  if (!oneItem) {
+    return (res.status(404).json({
+            status: 404,
+            message: "That company does not exist in the database",
+          }));
+  }
   const findCompanyId = oneItem.companyId;
   const findCompany = companies.find((company) => company._id == findCompanyId);
   if (findCompany) {
-    res.status(200).json({ data: findCompany });
+    res.status(200).json({ status: 200, message: "success", data: findCompany });
   } else {
-    res.status(400).json({
-      status: 400,
+    res.status(404).json({
+      status: 404,
       message: "That company does not exist in the database",
     });
   }
 };
 
-const getItems = (req, res) => {
-  const { sort_by, price, body_location } = req.query;
 
-  //We need to clone the array because we want to keep the original intact and keep our default order.
-  // This way of cloning an array is good only with array of JSON data.
-  let clonedItems = JSON.parse(JSON.stringify(items));
-  //sort and filter the items if needed
-  clonedItems = sortAndFilter(clonedItems, sort_by, price, body_location);
+const getItems = (req, res) => {    
+  const { sort_by, price, body_location } = req.query;  
+ 
+  //determine valid body location
+  let bodyLocationObject = {};
+  items.forEach(item => {
+    if (item.body_location)
+      bodyLocationObject[item.body_location.toLowerCase()] = true;
+  });
 
-  res.status(200).json({ status: 200, message: "success", data: clonedItems });
-};
+   //We need to clone the array because we want to keep the original intact and keep our default order. 
+  let clonedItems = JSON.parse(JSON.stringify(items)); 
+  //sort and filter the items if needed   
+  clonedItems = sortAndFilter(clonedItems, sort_by, price, body_location, bodyLocationObject); 
+  
+  res.status(200).json({ status: 200, message: "success", data: {items: clonedItems, bodyLocation: bodyLocationObject }});
+
+}
 
 const getCompagnies = (req, res) => {
   res.status(200).json({ status: 200, message: "success", data: companies });
 };
 
 const getItemsCategory = (req, res) => {
-  const { category } = req.params;
-  const { sort_by, price, body_location } = req.query;
-  // console.log(req.query);
 
-  if (!category)
-    return res
-      .status(400)
-      .json({ status: 400, message: "unknown category", data: { category } });
+    const { category } = req.params;   
+    const { sort_by, price, body_location } = req.query; 
+    console.log(req.query);
+   
+    if ( !category)
+      return res.status(400).json({ status: 400, message: "unknown category", data: {category} });
 
-  //filter the group by category
-  let itemsGroup = items.filter((item) => {
-    if (item.category)
-      return (
-        item.category.toLowerCase().replace(/\s/g, "") ===
-        category.toLowerCase()
-      );
-    return false;
-  });
+   //filter the group by category
+    let itemsGroup = items.filter((item)=>{    
+          if (item.category)    
+            return item.category.toLowerCase().replace(/\s/g, "") === category.toLowerCase();  
+          return false;     
+    });
 
-  if (itemsGroup.length === 0)
-    return res
-      .status(400)
-      .json({ status: 400, message: "category not found", data: { category } });
+    if (itemsGroup.length === 0)
+      return res.status(404).json({ status: 404, message: "category not found", data: {category} });
 
-  //sort and filter the items if needed
-  itemsGroup = sortAndFilter(itemsGroup, sort_by, price, body_location);
+    //determine valid body location for a category
+    let bodyLocationObject = {};  
+    itemsGroup.forEach(item => {   
+      if (item.body_location)
+        bodyLocationObject[item.body_location.toLowerCase()] = true;
+    });
 
-  return res
-    .status(200)
-    .json({ status: 200, message: "success", data: itemsGroup });
+    //sort and filter the items if needed
+    itemsGroup = sortAndFilter(itemsGroup, sort_by, price, body_location, bodyLocationObject);   
+    
+    return res.status(200).json({ status: 200, message: "success", data: {items: itemsGroup , bodyLocation: bodyLocationObject }});  
+
+    
+
 };
 
 // add a purchase "/purchase"
@@ -120,12 +137,9 @@ const addPurchase = (req, res) => {
     nB,
     newItems,
   };
-  console.log(purchase.newItems);
-  {
-    purchases.push(req.body);
-    fs.writeFileSync("./data/purchases.json", JSON.stringify(purchases));
+  
 
-    const itemsArray = purchase.newItems;
+    const itemsArray = [...purchase.newItems];
     itemsArray.forEach(myFunction);
 
     function myFunction(item, index, arr) {
@@ -133,7 +147,7 @@ const addPurchase = (req, res) => {
     }
     const value = "";
     errorArray = itemsArray.filter((item) => item !== value);
-    console.log(itemsArray);
+ 
     console.log(errorArray);
 
     if (errorArray.length > 0) {
@@ -144,22 +158,30 @@ const addPurchase = (req, res) => {
           itemsArray +
           " \r\n - - Please reduce the quantity and try again.",
       });
-    } else {
-      res.status(200).send({
-        status: 200,
-        data: purchase,
-      });
-    }
-  }
+    } else {     
+        purchases.push(purchase);
+        fs.writeFileSync("./data/purchases.json", JSON.stringify(purchases)); 
+        purchase.newItems.forEach((itemPurchase)=>{
+          items.forEach((item, index)=>{        
+            if (item._id === itemPurchase._id){              
+              items[index].numInStock -= itemPurchase.quantity;              
+            }
+          })
+        });
+        res.status(201).send({
+          status: 201,
+          data: purchase,
+        });
+    } 
+
 };
+
 
 module.exports = {
   getSingleItem,
   getCompanyById,
   getCompagnies,
   getItems,
-  //getItemsGroup,
   addPurchase,
   getItemsCategory,
-  //getItemBySearch
 };
